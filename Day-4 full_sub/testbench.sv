aa199e0f7100820ca421f736f802d6598bf42b6d
Sulekha <96179625+Sulekha231@users.noreply.github.com>
2023-04-20 23:57:20 +0530

    Create testbench.sv

---
+++ Day-4 full_sub/testbench.sv
@@ -0,0 +1,29 @@
+//testbench for subtractor
+module tb;
+  reg A,B,C;
+  wire D,BO;
+  
+  full_sub  fs(.a(A), .b(B), .c(C), .d(D), .bo(BO));
+  
+  initial
+    begin
+      
+      $monitor("A=%b, B=%b, C=%b, D=%b, BO=%b",A,B,C,D,BO,$time);
+     A=0; B=0; C=0;
+   #2A=0; B=0; C=1;
+   #2A=0; B=1; C=0;
+   #2A=0; B=1; C=1;
+   #2A=1; B=0; C=0;
+   #2A=1; B=0; C=1;
+   #2A=1; B=1; C=0;
+   #2A=1; B=1; C=1;    
+     
+    end
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+    end
+  
+endmodule
