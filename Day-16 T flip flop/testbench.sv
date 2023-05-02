928b23075aa0084712cf0ccb0eef0eabd53a8880
Sulekha <96179625+Sulekha231@users.noreply.github.com>
2023-05-02 14:15:34 +0530

    Create testbench.sv

---
+++ Day-16 T flip flop/testbench.sv
@@ -0,0 +1,33 @@
+//testbench for T flip flop
+module tb;
+  reg CLK,T,RESET;
+  wire Q;
+  
+  t_ff inst(CLK,T,RESET,Q);
+  
+   initial
+    begin
+      CLK=0;
+      forever #2CLK=~CLK;
+      
+    end
+  
+      
+  initial
+    begin
+      $monitor("CLK=%b,  RESET=%b, T=%b, Q=%b",CLK,RESET,T,Q,$time);
+      #2RESET=0;  T=0;
+      #4RESET=0; T=1;
+     #4RESET=1; T=0;
+      
+    end
+  
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+      #12$finish;
+    end
+  
+      
+endmodule
