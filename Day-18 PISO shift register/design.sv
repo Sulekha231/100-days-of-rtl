134975f201f95ab4ba5201c4aa65f13c68b38fc9
Sulekha <96179625+Sulekha231@users.noreply.github.com>
2023-05-06 19:21:56 +0530

    Create design.sv

---
+++ Day-18 PISO shift register/design.sv
@@ -0,0 +1,23 @@
+module piso(input [3:0]in,clk,reset, output reg out);
+  reg [3:0] t;
+  
+  initial
+    t=in;
+  
+  always@(posedge clk)
+    begin
+      if(reset)
+        begin
+        out<=0;
+         t<=in;
+        end
+      
+      else
+        begin
+          out<=t[0];
+          t<= t>>1;
+        end
+      
+    end     
+  
+endmodule
