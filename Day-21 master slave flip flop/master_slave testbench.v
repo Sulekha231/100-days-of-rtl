47987574b20009b7229d4b2d188253e0a185461b
Sulekha <96179625+Sulekha231@users.noreply.github.com>
2023-05-10 19:57:37 +0530

    Update master_slave testbench.v

--- Day-21 master slave flip flop/master_slave testbench.v
+++ Day-21 master slave flip flop/master_slave testbench.v
@@ -16,7 +16,7 @@ module tb;
     begin
       $monitor("CLK=%b, JM=%b, KM=%b, QS=%b, QS_BAR=%b",CLK,JM,KM,QS,QS_BAR,$time);
         JM=0; KM=0; 
-      #2JM=0; KM=1;
+      #4JM=0; KM=1;
       #4JM=1; KM=0;
       #4JM=1; KM=1;
     end
